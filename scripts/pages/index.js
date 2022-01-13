7da29830f582a6c07b36c32df3a8ce9ba2acaf42
Quentin De Andrade <deandradeq@gmail.com>
2022-01-13 23:20:16 +0100

    Affichage des photographes, travail sur index.js et ajout de la function async getPhotographers

---
+++ scripts/pages/index.js
@@ -0,0 +1,26 @@
+    async function getPhotographers() {
+        // Penser à remplacer par les données récupérées dans le json
+        let data = await fetch("data/photographers.json");
+
+        let photographes = await data.json();
+
+        return photographes.photographers;
+    }
+
+    function displayData(photographers) {
+        const photographersSection = document.querySelector(".photographer_section");
+
+        photographers.forEach((photographer) => {
+            const photographerModel = photographerFactory(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersSection.appendChild(userCardDOM);
+        });
+    };
+
+    async function init() {
+        // Récupère les datas des photographes
+        const photographers = await getPhotographers();
+        displayData(photographers);
+    };
+    
+    init();
