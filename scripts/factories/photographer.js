a66d494323ab0ca52246c99d761e1de9a6a6de1b
Quentin De Andrade <deandradeq@gmail.com>
2022-04-08 16:40:13 +0200

    Infos photographe sur index + lightbox fullscreen

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait, id } = data;
+    const { name, portrait, id, city,  country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,6 +7,15 @@ function photographerFactory(data) {
         const a = document.createElement('a');
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        const span = document.createElement('span')
+        const p = document.createElement('p')
+        const spanPrice = document.createElement('span')
+        spanPrice.classList.add('indexprice')
+        spanPrice.textContent = price + '€/jour'
+        p.textContent = tagline
+        span.textContent = city + ', ' + country
+        span.classList.add('cityCountry')
+        img.classList.add('round')
         img.setAttribute("src", picture)
         img.setAttribute("alt", name)
         const h2 = document.createElement( 'h2' );
@@ -15,6 +24,9 @@ function photographerFactory(data) {
         a.appendChild(article);
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(span)
+        article.appendChild(p)
+        article.appendChild(spanPrice)
         return a;
     }
     return { name, picture, getUserCardDOM }
